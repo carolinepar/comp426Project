c8194d567426cff834c24d75b0cfb505b55136d7
Sam Auld <samuelaauld@gmail.com>
2019-12-06 22:29:11 -0500

    updated styling, made account page

--- render.js
+++ render.js
@@ -1,5 +1,6 @@
 export const renderSite = function() {
     const $root = $('#root');
+    $root.empty();
     $root.append(`
     <section class="hero is-info is-bold">
         <div class="hero-body">
@@ -9,14 +10,16 @@ export const renderSite = function() {
             </div>
         </div>
     </section>
+    <button class="button account-button" style="margin-left:2%; margin-top:15px;">Account</button>
     <section class="section">
-        <div>
-            <div class="control">
-                    <input class="input" type="text" placeholder="Search threads">
+        <div class="box" style="background-color: #209CEE;">
+            <h1 class="title" style="color:white;">Threads</h1>
+            <div>
+                <div class="control">
+                        <input class="input" type="text" placeholder="Search threads">
+                </div>
             </div>
-        </div>
-        <div class="box">
-            <h1 class="title">Threads</h1>
+            <br>
             <div id="threadFeed">
                 <div class="box">
                     <h3 class="title is-4">Post Title Placeholder</h3>
@@ -30,9 +33,12 @@ export const renderSite = function() {
     renderFeed();
 }
 
-export const renderPostSite = function() {
+export const renderPost = function() {
     const $root = $('#root');
     $root.empty();
+
+    //make axios call to get thread specific info
+
     $root.append(`
     <section class="hero is-info is-bold">
         <div class="hero-body">
@@ -42,14 +48,14 @@ export const renderPostSite = function() {
         </div>
     </section>
     <section>
-        <!-- not sure if below is how we want to do the back button-->
-        <button onclick="window.location.href = 'http://localhost:3000';" class="button" style="margin-left:4%; margin-top:5px;">Back</button>
+        <button class="button back-button" style="margin-left:4%; margin-top:15px;">Back</button>
+        <br>
         <h1 class="title is-3" id="threadTitle">Title Placeholder</h1>
         <div class="box" style="margin-left: 5%; margin-right:5%;">
             <h1 class="title is-4">Author Name</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
-        <div id="commentFeed" class="box" style="background-color: #00B2D6; margin-left: 5%; margin-right:5%;">
+        <div id="commentFeed" class="box" style="background-color: #209CEE; margin-left: 5%; margin-right:5%;">
             <h1 class="title is-4" style="color: white;">Replies</h1>
             <div class="box">
                 <h1 class="subtitle is-6">Commenter Name</h1>
@@ -67,17 +73,94 @@ export const renderPostSite = function() {
         <br>
     </section>
     `);
+
+    renderComments();
+}
+
+export const renderAccount = function() {
+    const $root = $('#root');
+    $root.empty();
+
+    $root.append(`
+    <section class="hero is-info is-bold">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">426Sports</h1>
+            </div>
+        </div>
+    </section>
+    <section>
+        <button class="button back-button" style="margin-left:4%; margin-top:15px;">Back</button>
+        <br>
+        <br>
+        <div style="margin-left: 5%; margin-top:5px;">
+            <h1 class="title">Account Name</h1>
+        </div>
+        <br>
+        <br>
+        <div class="tile is-ancestor">
+            <div class="tile is-child is-1"></div>
+            <div class="tile is-child box" style="background-color:#209CEE;">
+                <div>
+                    <h1 class="title is-3" style="color:white;">Your Posts</h1>
+                    <div class="box">
+                        <h3 class="title is-4">Post Title Placeholder</h3>
+                        <h3 class="subtitle is-6"><strong>Sport Placeholder</strong></h5>
+                        <button class="button view-button">View</button>
+                    </div>
+                    <div class="box">
+                        <h3 class="title is-4">Post Title Placeholder</h3>
+                        <h3 class="subtitle is-6"><strong>Sport Placeholder</strong></h5>
+                        <button class="button view-button">View</button>
+                    </div>
+                </div>
+            </div>
+            <div class="tile is-child is-1"></div>
+            <div class="tile is-child box" style="background-color:#209CEE;">
+                <div>
+                    <h1 class="title is-3" style="color:white;">Favorite Posts</h1>
+                    <div class="box">
+                        <h3 class="title is-4">Post Title Placeholder</h3>
+                        <h3 class="subtitle is-6">Author Name Placeholder &emsp; <strong>Sport Placeholder</strong></h5>
+                        <button class="button view-button">View</button>
+                    </div>
+                </div>
+            </div>
+            <div class="tile is-child is-1"></div>
+        </div>
+    </section>
+    `);
+
+    //make render functions
 }
 
 export async function renderFeed() {
-    const $threadFeed = $('#threadFeed');
+    const $threads = $('#threadFeed');
     //make axios call to get all the threads, loop through and append to the threadFeed div
+}
+
+export async function renderComments() {
+    const $comments = $('#commentFeed');
+    //make axios call to get all the threads, loop through and append to the threadFeed div
+
 }
 
 export const handleViewButtonEvent = function(event) {
     event.preventDefault();
 
-    renderPostSite();
+    renderPost();
+}
+
+export const handleBackButtonEvent = function(event) {
+    event.preventDefault();
+
+    renderSite();
+}
+
+export const handleAccountButtonEvent = function(event) {
+    event.preventDefault();
+
+    renderAccount();
 }
 
 $(function() {
@@ -86,4 +169,8 @@ $(function() {
     const $root = $('#root');
 
     $root.on('click', '.view-button', handleViewButtonEvent);
+
+    $root.on('click', '.back-button', handleBackButtonEvent);
+
+    $root.on('click', '.account-button', handleAccountButtonEvent);
 });
